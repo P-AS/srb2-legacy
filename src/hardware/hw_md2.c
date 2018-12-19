eb14cbac13862b0a9749a096fc2efb1d291fee0f
mazmazz <mar.marcoz@outlook.com>
2018-12-19 10:33:13 -0500

    Ifdef nextFrame handling under USE_MODEL_NEXTFRAME

--- src/hardware/hw_md2.c
+++ src/hardware/hw_md2.c
@@ -1035,8 +1035,7 @@ void HWR_DrawMD2(gr_vissprite_t *spr)
 		//FIXME: this is not yet correct
 		frame = (spr->mobj->frame & FF_FRAMEMASK) % md2->model->meshes[0].numFrames;
 
-#if 0
-
+#ifdef USE_MODEL_NEXTFRAME
 		if (cv_grmd2.value == 1 && tics <= durs)
 		{
 			// frames are handled differently for states with FF_ANIMATE, so get the next frame differently for the interpolation
@@ -1058,7 +1057,6 @@ void HWR_DrawMD2(gr_vissprite_t *spr)
 				}
 			}
 		}
-
 #endif
 
 		//Hurdler: it seems there is still a small problem with mobj angle
