403a91d7ff74b71b120bb263bad9f659097b196d
GoldenTails <milestailsprower101n2@gmail.com>
2019-10-08 22:03:43 -0500

    Create V_DrawCenteredSmallThinStringAtFixed() for new "small-thin-fixed-center" option in v.drawString()
    Thankfully "center" is just "right" but with the X offset divided by 2.

--- src/lua_hudlib.c
+++ src/lua_hudlib.c
@@ -110,6 +110,7 @@ enum align {
 	align_smallright,
 	align_smallthin,
 	align_smallthinfixed,
+	align_smallthinfixedcenter,
 	align_smallthinfixedright,
 	align_thin,
 	align_thinfixed,
@@ -133,6 +134,7 @@ static const char *const align_opt[] = {
 	"small-right",
 	"small-thin",
 	"small-thin-fixed",
+	"small-thin-fixed-center",
 	"small-thin-fixed-right",
 	"thin",
 	"thin-fixed",
@@ -506,6 +508,9 @@ static int libd_drawString(lua_State *L)
 		break;
 	case align_smallthinfixed:
 		V_DrawSmallThinStringAtFixed(x, y, flags, str);
+		break;
+	case align_smallthinfixedcenter:
+		V_DrawCenteredSmallThinStringAtFixed(x, y, flags, str);
 		break;
 	case align_smallthinfixedright:
 		V_DrawRightAlignedSmallThinStringAtFixed(x, y, flags, str);
