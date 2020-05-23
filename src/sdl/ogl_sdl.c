cb144b066c3be68d9c290010a6fd89ae79b3dde4
Steel Titanium <steeltitanium1@gmail.com>
2020-05-23 16:21:26 -0400

    Fix opengl crashing on startup due to lack of checks
    Also fixes the log file not being written to the home directory.

--- src/sdl/ogl_sdl.c
+++ src/sdl/ogl_sdl.c
@@ -33,6 +33,7 @@
 #endif
 
 #include "../doomdef.h"
+#include "../d_main.h"
 
 #ifdef HWRENDER
 #include "../hardware/r_opengl/r_opengl.h"
@@ -108,6 +109,22 @@ boolean OglSdlSurface(INT32 w, INT32 h)
 {
 	INT32 cbpp = cv_scr_depth.value < 16 ? 16 : cv_scr_depth.value;
 	static int majorGL = 0, minorGL = 0;
+	const char *gllogdir = NULL;
+
+
+	if (!gllogstream)
+	{
+		gllogdir = D_Home();
+
+#ifdef DEBUG_TO_FILE
+#ifdef DEFAULTDIR
+		if (gllogdir)
+				gllogstream = fopen(va("%s/"DEFAULTDIR"/ogllog.txt",gllogdir), "wt");
+		else
+#endif
+			gllogstream = fopen("./ogllog.txt", "wt");
+#endif
+	}
 
 	// Get info and extensions.
 	//BP: why don't we make it earlier ?
