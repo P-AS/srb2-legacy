d20555fb92450601c22cc39a3d5b503242338eed
James R <justsomejames2@gmail.com>
2020-09-01 15:37:32 -0700

    Define BETAVERSION as a suffix to the version string

--- src/version.h
+++ src/version.h
@@ -10,3 +10,6 @@
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
 #define MODVERSION 33
+
+// Define this as a prerelease version suffix
+//#define BETAVERSION "RC1"
