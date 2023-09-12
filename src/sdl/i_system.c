9aee23ccab0031eeac35eab83f2ec27065a841da
Alam Ed Arias <alam@srb2.org>
2023-09-12 19:32:47 -0400

    SDL: add version check for SDL_OPenURL()

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -417,8 +417,10 @@ static void I_ReportSignal(int num, int coredumped)
 
 	SDL_ShowMessageBox(&messageboxdata, &buttonid);
 
+#if SDL_VERSION_ATLEAST(2,0,14)
 	if (buttonid == 1)
 		SDL_OpenURL("https://github.com/P-AS/srb2-legacy/issues");
+#endif
 }
 
 #ifndef NEWSIGNALHANDLER
