961de8ab934f09e17b77295d1043fe7c73370707
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2024-12-10 20:21:52 -0600

    Fix console movement being slow when frame interp was turned off

--- src/console.c
+++ src/console.c
@@ -533,7 +533,10 @@ static void CON_MoveConsole(void)
 	}
 
 	// Not instant - Increment fracmovement fractionally
+	if (cv_frameinterpolation.value == 1)
 	fracmovement += FixedMul(cons_speed.value*vid.fdupy, I_GetTimeFrac());
+	else 
+	fracmovement += FixedMul(cons_speed.value*vid.fdupy, FRACUNIT);
 
 	if (con_curlines < con_destlines) // Move the console downwards
 	{
