b099a137835c54d44a498e40c83a8ff349293338
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2024-12-12 17:49:34 -0600

    Fix GL Sprite shadows on slopes
    Thanks SRB2Kart Saturn!!!!!!

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4079,6 +4079,10 @@ static void HWR_DrawSpriteShadow(gr_vissprite_t *spr, GLPatch_t *gpatch, float t
 	FSurfaceInfo sSurf;
 	fixed_t floorheight, mobjfloor;
 	float offset = 0;
+	pslope_t *floorslope;
+	fixed_t slopez;
+
+	R_GetShadowZ(spr->mobj, &floorslope);
 
 	mobjfloor = HWR_OpaqueFloorAtPos(
 		spr->mobj->x, spr->mobj->y,
@@ -4167,6 +4171,13 @@ static void HWR_DrawSpriteShadow(gr_vissprite_t *spr, GLPatch_t *gpatch, float t
 		swallVerts[1].x = spr->x2 + offset * gr_viewcos;
 		swallVerts[0].z = spr->z1 + offset * gr_viewsin;
 		swallVerts[1].z = spr->z2 + offset * gr_viewsin;
+	}
+
+	if (floorslope)
+	for (int i = 0; i < 4; i++)
+	{
+		slopez = P_GetZAt(floorslope, FLOAT_TO_FIXED(swallVerts[i].x), FLOAT_TO_FIXED(swallVerts[i].z));
+		swallVerts[i].y = FIXED_TO_FLOAT(slopez) + 0.05f;
 	}
 
 	if (spr->flip)
