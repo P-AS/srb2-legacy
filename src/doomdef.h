afd013bf670e6bc8ba894b89ddad4f35bc839527
PAS <pasrc@proton.me>
2025-01-10 20:24:38 -0500

    Update version to 2.1.27

--- src/doomdef.h
+++ src/doomdef.h
@@ -150,10 +150,10 @@ extern FILE *logstream;
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    201 // Game version
-#define SUBVERSION 26  // more precise version number
+#define SUBVERSION 27  // more precise version number
 #define SUBVERSION_NETCOMPAT 25  // for backwards compatibility with 2.1.25 servers
-#define VERSIONSTRING "v2.1.26"
-#define VERSIONSTRINGW L"v2.1.26"
+#define VERSIONSTRING "v2.1.27"
+#define VERSIONSTRINGW L"v2.1.27"
 // Hey! If you change this, add 1 to the MODVERSION below!
 // Otherwise we can't force updates!
 #endif
@@ -218,7 +218,7 @@ extern FILE *logstream;
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
-#define MODVERSION 31
+#define MODVERSION 32
 
 // To version config.cfg, MAJOREXECVERSION is set equal to MODVERSION automatically.
 // Increment MINOREXECVERSION whenever a config change is needed that does not correspond
