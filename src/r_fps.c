2e24c1c40559eb0e6f9c0d0c76710a9377eeaf51
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-02-01 15:12:24 -0600

    dont interpolate if frac = FRACUNIT

--- src/r_fps.c
+++ src/r_fps.c
@@ -248,11 +248,20 @@ void R_SetViewContext(enum viewcontext_e _viewcontext)
 
 void R_InterpolateMobjState(mobj_t *mobj, fixed_t frac, interpmobjstate_t *out)
 {
+	if (frac == FRACUNIT)
+	{
+		out->x = mobj->x;
+		out->y = mobj->y;
+		out->z = mobj->z;
+		out->scale = mobj->scale;
+		out->angle = mobj->angle;
+		return;
+	}
 	out->x =  R_LerpFixed(mobj->old_x, mobj->x, frac);
 	out->y =  R_LerpFixed(mobj->old_y, mobj->y, frac);
 	out->z =  R_LerpFixed(mobj->old_z, mobj->z, frac);
 	out->angle = mobj->resetinterp ? mobj->angle : R_LerpAngle(mobj->old_angle, mobj->angle, frac);
-	if (mobj->scale == mobj->old_scale) // Tiny optimisation - scale is usually unchanging, so let's skip a lerp, two FixedMuls, and two FixedDivs
+	if (mobj->scale == mobj->old_scale) // Tiny optimisation - scale is usually unchanging, so let's skip a lerp
 	{
 		out->scale = mobj->scale;
 	}
