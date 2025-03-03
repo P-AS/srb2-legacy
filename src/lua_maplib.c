9e661d9d6890b318de411d502373ee12638be958
Gustaf Alhäll <gustaf@hanicef.me>
2025-03-03 18:35:51 +0100

    Properly fix Mac OS X build

--- src/lua_maplib.c
+++ src/lua_maplib.c
@@ -1539,6 +1539,7 @@ static int mapheaderinfo_get(lua_State *L)
 	mapheader_t *header = *((mapheader_t **)luaL_checkudata(L, 1, META_MAPHEADER));
 	enum mapheaderinfo_e field = Lua_optoption(L, 2, -1, mapheaderinfo_fields_ref);
 	INT16 i;
+	UINT8 j;
 	switch (field)
 	{
 	case mapheaderinfo_lvlttl:
@@ -1638,8 +1639,7 @@ static int mapheaderinfo_get(lua_State *L)
 	default:
 		// Read custom vars now
 		// (note: don't include the "LUA." in your lua scripts!)
-		UINT32 j = 0;
-		for (;j < header->numCustomOptions && !fastcmp(lua_tostring(L, 2), header->customopts[j].option); ++j);
+		for (j = 0; j < header->numCustomOptions && !fastcmp(lua_tostring(L, 2), header->customopts[j].option); ++j);
 
 		if(j < header->numCustomOptions)
 			lua_pushstring(L, header->customopts[j].value);
