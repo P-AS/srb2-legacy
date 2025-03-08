aed076e0a046500cbf5ab391cc946ae6c12813e1
PAS <pasrc@proton.me>
2025-03-07 23:37:36 -0500

    Use inputflags variable for input display

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -777,7 +777,6 @@ static void ST_drawLives(void)
 	V_DrawRightAlignedString(hudinfo[HUD_LIVESNUM].x, hudinfo[HUD_LIVESNUM].y + (v_splitflag ? -4 : 0), hudinfo[HUD_LIVESNUM].flags|V_HUDTRANS|v_splitflag, va("%d",stplyr->lives));
 }
 
-
 static void ST_drawInput(void)
 {
 	INT32 accent = V_SNAPTOLEFT|V_SNAPTOBOTTOM;
@@ -890,8 +889,9 @@ static void ST_drawInput(void)
 			break;
 	}
 
-
-	INT32 x = hudinfo[HUD_INPUT].x, y = hudinfo[HUD_INPUT].y;
+	INT32 x = hudinfo[HUD_INPUT].x;
+	INT32 y = hudinfo[HUD_INPUT].y;
+	INT32 inputflags = hudinfo[HUD_INPUT].flags;
 
 	if(stplyr->pflags & PF_NIGHTSMODE)
 	y += 8;
@@ -909,23 +909,23 @@ static void ST_drawInput(void)
 		y -= 5;
 
 	// O backing
-	V_DrawFill(x, y-1, 16, 16, V_SNAPTOLEFT|V_SNAPTOBOTTOM|20);
-	V_DrawFill(x, y+15, 16, 1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+	V_DrawFill(x, y-1, 16, 16, inputflags|20);
+	V_DrawFill(x, y+15, 16, 1, inputflags|29);
 
 	if (cv_showinputjoy.value) // joystick render!
 	{
-		/*V_DrawFill(x   , y   , 16,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16);
-		V_DrawFill(x   , y+15, 16,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16);
-		V_DrawFill(x   , y+ 1,  1, 14, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16);
-		V_DrawFill(x+15, y+ 1,  1, 14, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16); -- red's outline*/
+		/*V_DrawFill(x   , y   , 16,  1, inputflags|16);
+		V_DrawFill(x   , y+15, 16,  1, inputflags|16);
+		V_DrawFill(x   , y+ 1,  1, 14, inputflags|16);
+		V_DrawFill(x+15, y+ 1,  1, 14, inputflags|16); -- red's outline*/
 		if (stplyr->cmd.sidemove || stplyr->cmd.forwardmove)
 		{
 			// joystick hole
-			V_DrawFill(x+5, y+4, 6, 6, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			V_DrawFill(x+5, y+4, 6, 6, inputflags|29);
 			// joystick top
 			V_DrawFill(x+3+stplyr->cmd.sidemove/12,
 				y+2-stplyr->cmd.forwardmove/12,
-				10, 10, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+				10, 10, inputflags|29);
 			V_DrawFill(x+3+stplyr->cmd.sidemove/9,
 				y+1-stplyr->cmd.forwardmove/9,
 				10, 10, accent);
@@ -933,10 +933,10 @@ static void ST_drawInput(void)
 		else
 		{
 			// just a limited, greyed out joystick top
-			V_DrawFill(x+3, y+11, 10, 1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			V_DrawFill(x+3, y+11, 10, 1, inputflags|29);
 			V_DrawFill(x+3,
 				y+1,
-				10, 10, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16);
+				10, 10, inputflags|16);
 		}
 	}
 	else // arrows!
@@ -950,10 +950,10 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = V_SNAPTOLEFT|V_SNAPTOBOTTOM|16;
-			V_DrawFill(x- 2, y+10,  6,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+ 4, y+ 9,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+ 5, y+ 8,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			col = inputflags|16;
+			V_DrawFill(x- 2, y+10,  6,  1, inputflags|29);
+			V_DrawFill(x+ 4, y+ 9,  1,  1, inputflags|29);
+			V_DrawFill(x+ 5, y+ 8,  1,  1, inputflags|29);
 		}
 		V_DrawFill(x- 2, y+ 5-offs,  6,  6, col);
 		V_DrawFill(x+ 4, y+ 6-offs,  1,  4, col);
@@ -968,12 +968,12 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = V_SNAPTOLEFT|V_SNAPTOBOTTOM|16;
-			V_DrawFill(x+ 5, y+ 3,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+ 6, y+ 4,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+ 7, y+ 5,  2,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+ 9, y+ 4,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+10, y+ 3,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			col = inputflags|16;
+			V_DrawFill(x+ 5, y+ 3,  1,  1, inputflags|29);
+			V_DrawFill(x+ 6, y+ 4,  1,  1, inputflags|29);
+			V_DrawFill(x+ 7, y+ 5,  2,  1, inputflags|29);
+			V_DrawFill(x+ 9, y+ 4,  1,  1, inputflags|29);
+			V_DrawFill(x+10, y+ 3,  1,  1, inputflags|29);
 		}
 		V_DrawFill(x+ 5, y- 2-offs,  6,  6, col);
 		V_DrawFill(x+ 6, y+ 4-offs,  4,  1, col);
@@ -988,10 +988,10 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = V_SNAPTOLEFT|V_SNAPTOBOTTOM|16;
-			V_DrawFill(x+12, y+10,  6,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+11, y+ 9,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
-			V_DrawFill(x+10, y+ 8,  1,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			col = inputflags|16;
+			V_DrawFill(x+12, y+10,  6,  1, inputflags|29);
+			V_DrawFill(x+11, y+ 9,  1,  1, inputflags|29);
+			V_DrawFill(x+10, y+ 8,  1,  1, inputflags|29);
 		}
 		V_DrawFill(x+12, y+ 5-offs,  6,  6, col);
 		V_DrawFill(x+11, y+ 6-offs,  1,  4, col);
@@ -1006,15 +1006,15 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = V_SNAPTOLEFT|V_SNAPTOBOTTOM|16;
-			V_DrawFill(x+ 5, y+17,  6,  1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);
+			col = inputflags|16;
+			V_DrawFill(x+ 5, y+17,  6,  1, inputflags|29);
 		}
 		V_DrawFill(x+ 5, y+12-offs,  6,  6, col);
 		V_DrawFill(x+ 6, y+11-offs,  4,  1, col);
 		V_DrawFill(x+ 7, y+10-offs,  2,  1, col);
 	}
 
-#define drawbutt(xoffs, yoffs, butt, symb)\
+#define drawbuttons(xoffs, yoffs, butt, symb)\
 	if (stplyr->cmd.buttons & butt)\
 	{\
 		offs = 0;\
@@ -1023,16 +1023,16 @@ static void ST_drawInput(void)
 	else\
 	{\
 		offs = 1;\
-		col = V_SNAPTOLEFT|V_SNAPTOBOTTOM|16;\
-		V_DrawFill(x+16+(xoffs), y+9+(yoffs), 10, 1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|29);\
+		col = inputflags|16;\
+		V_DrawFill(x+16+(xoffs), y+9+(yoffs), 10, 1, inputflags|29);\
 	}\
 	V_DrawFill(x+16+(xoffs), y+(yoffs)-offs, 10, 10, col);\
-	V_DrawCharacter(x+16+1+(xoffs), y+1+(yoffs)-offs, V_SNAPTOLEFT|V_SNAPTOBOTTOM|symb, false)
+	V_DrawCharacter(x+16+1+(xoffs), y+1+(yoffs)-offs, inputflags|symb, false)
 
-	drawbutt( 4,-3, BT_JUMP, 'J');
-	drawbutt(15,-3, BT_USE,  'S');
+	drawbuttons( 4,-3, BT_JUMP, 'J');
+	drawbuttons(15,-3, BT_USE,  'S');
 
-	V_DrawFill(x+16+4, y+8, 21, 10, V_SNAPTOLEFT|V_SNAPTOBOTTOM|20); // sundial backing
+	V_DrawFill(x+16+4, y+8, 21, 10, inputflags|20); // sundial backing
 	if (stplyr->mo)
 	{
 		UINT8 i, precision;
@@ -1052,14 +1052,14 @@ static void ST_drawInput(void)
 		{
 			V_DrawFill(x+16+14-(i*xcomp)/precision,
 				y+12-(i*ycomp)/precision,
-				1, 1, V_SNAPTOLEFT|V_SNAPTOBOTTOM|16);
+				1, 1, inputflags|16);
 		}
 
 		if (ycomp <= 0)
 			V_DrawFill(x+16+13-xcomp, y+11-ycomp, 3, 3, accent); // point (in front)
 	}
 
-#undef drawbutt
+#undef drawbuttons
 
 	// text above
 	x -= 2;
@@ -1067,15 +1067,12 @@ static void ST_drawInput(void)
 
 		if (stplyr->pflags & PF_ANALOGMODE)
 		{
-			V_DrawThinString(x, y, V_SNAPTOLEFT|V_SNAPTOBOTTOM, "ANALOG");
+			V_DrawThinString(x, y, inputflags, "ANALOG");
 			y -= 8;
 		}
 	if (!demosynced) // should always be last, so it doesn't push anything else around
 		V_DrawThinString(x, y, V_SNAPTOLEFT|V_SNAPTOBOTTOM|((leveltime & 4) ? V_YELLOWMAP : V_REDMAP), "BAD DEMO!!");
 }
-
-
-
 
 static void ST_drawLevelTitle(void)
 {
