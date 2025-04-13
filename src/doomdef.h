e9577f0936b883c70d3021ebe348d166211fa144
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-04-13 16:06:17 -0500

    Add view rolling

--- src/doomdef.h
+++ src/doomdef.h
@@ -419,6 +419,8 @@ extern INT32 cv_debug;
 #define DBG_MEMORY      0x0200
 #define DBG_SETUP       0x0400
 #define DBG_LUA         0x0800
+#define DBG_VIEWMORPH   0x2000
+
 
 // =======================
 // Misc stuff for later...
