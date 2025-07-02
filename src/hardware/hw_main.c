4f6647bc54829326e9e786c38aba05d0b9b12324
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-07-02 16:48:45 -0500

    Fix Signpost sorting issues in OpenGL

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4059,6 +4059,7 @@ static void HWR_SplitSprite(gr_vissprite_t *spr)
 	HWR_RotateSpritePolyToAim(spr, baseWallVerts);
 
 	// push it toward the camera to mitigate floor-clipping sprites
+	if (spr->mobj->type != MT_SIGN) // but not for goalposts
 	{
 		float sprdist = sqrtf((spr->x1 - gr_viewx)*(spr->x1 - gr_viewx) + (spr->z1 - gr_viewy)*(spr->z1 - gr_viewy) + (spr->ty - gr_viewz)*(spr->ty - gr_viewz));
 		float distfact = ((2.0f*spr->dispoffset) + 20.0f) / sprdist;
@@ -4348,6 +4349,7 @@ static void HWR_DrawSprite(gr_vissprite_t *spr)
 	HWR_RotateSpritePolyToAim(spr, wallVerts);
 
 	// push it toward the camera to mitigate floor-clipping sprites
+	if (spr->mobj->type != MT_SIGN)
 	{
 		float sprdist = sqrtf((spr->x1 - gr_viewx)*(spr->x1 - gr_viewx) + (spr->z1 - gr_viewy)*(spr->z1 - gr_viewy) + (spr->ty - gr_viewz)*(spr->ty - gr_viewz));
 		float distfact = ((2.0f*spr->dispoffset) + 20.0f) / sprdist;
