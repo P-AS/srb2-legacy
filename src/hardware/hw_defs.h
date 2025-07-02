7e0631e683b7c20c0b7155f4276bc9c33f7014db
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-07-02 16:06:48 -0500

    (Disabled) Light table support for HWR_FadeScreenMenuBack

--- src/hardware/hw_defs.h
+++ src/hardware/hw_defs.h
@@ -157,6 +157,7 @@ enum
 	SHADER_FOG,
 	SHADER_SKY,
 	SHADER_PALETTE_POSTPROCESS,
+	SHADER_UI_COLORMAP_FADE,
 
 	NUMSHADERTARGETS,
 };
@@ -349,7 +350,7 @@ enum hwdscreentexture
 	HWD_SCREENTEXTURE_WIPE_START, // source image for the wipe/fade effect
 	HWD_SCREENTEXTURE_WIPE_END,   // destination image for the wipe/fade effect
 	HWD_SCREENTEXTURE_GENERIC1,   // underwater/heat effect, intermission background
-	HWD_SCREENTEXTURE_GENERIC2,   // screen before palette rendering's postprocessing
+	HWD_SCREENTEXTURE_GENERIC2,   // palette-based colormap fade, screen before palette rendering's postprocessing
 	HWD_SCREENTEXTURE_GENERIC3,   // screen after palette rendering's postprocessing
 	NUMSCREENTEXTURES,            // (generic3 is unused if palette rendering is disabled)
 };
