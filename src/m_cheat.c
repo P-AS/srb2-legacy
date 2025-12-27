P-AS/srb2-legacy

commit 9ea0060b67935ba5eceebeadb96b7f5b90c90817

SERIOUS Objectplace improvement.
* Object number up/down is now assigned to ringslinger weapon next/prev. SO much nicer to use, especially since most people have that stuff bound to the scroll wheel.

src/m_cheat.c
@@ -1150,23 +1150,17 @@ void OP_ObjectplaceMovement(player_t *player)
 	if (player->pflags & PF_ATTACKDOWN)
 	{
 		// Are ANY objectplace buttons pressed?  If no, remove flag.
-		if (!(cmd->buttons & (BT_ATTACK|BT_TOSSFLAG|BT_CAMRIGHT|BT_CAMLEFT)))
+		if (!(cmd->buttons & (BT_ATTACK|BT_TOSSFLAG)))
 			player->pflags &= ~PF_ATTACKDOWN;
 
 		// Do nothing.
 		return;
 	}
 
-	if (cmd->buttons & BT_CAMLEFT)
-	{
+	if (cmd->buttons & BT_WEAPONPREV)
 		OP_CycleThings(-1);
-		player->pflags |= PF_ATTACKDOWN;
-	}
-	else if (cmd->buttons & BT_CAMRIGHT)
-	{
+	else if (cmd->buttons & BT_WEAPONNEXT)
 		OP_CycleThings(1);
-		player->pflags |= PF_ATTACKDOWN;
-	}
 
 	// Place an object and add it to the maplist
 	if (cmd->buttons & BT_ATTACK)
@@ -1249,7 +1243,7 @@ void Command_ObjectPlace_f(void)
 			HU_DoCEcho(va(M_GetText(
 				"\\\\\\\\\\\\\\\\\\\\\\\\\x82"
 				"   Objectplace Controls:   \x80\\\\"
-				"Camera L/R: Cycle mapthings\\"
+				"Weapon L/R: Cycle mapthings\\"
 				"      Jump: Float up       \\"
 				"      Spin: Float down     \\"
 				" Fire Ring: Place object   \\")));

commit 62a7aeb12276de61259e2dfb5ef5c69c262ee21a

Fix objectplace next/prev scrolling too fast
Regression from 9ea0060b67935ba5eceebeadb96b7f5b90c90817, fixes #152

src/m_cheat.c
@@ -1158,9 +1158,15 @@ void OP_ObjectplaceMovement(player_t *player)
 	}
 
 	if (cmd->buttons & BT_WEAPONPREV)
+	{
 		OP_CycleThings(-1);
+		player->pflags |= PF_ATTACKDOWN;
+	}
 	else if (cmd->buttons & BT_WEAPONNEXT)
+	{
 		OP_CycleThings(1);
+		player->pflags |= PF_ATTACKDOWN;
+	}
 
 	// Place an object and add it to the maplist
 	if (cmd->buttons & BT_ATTACK)