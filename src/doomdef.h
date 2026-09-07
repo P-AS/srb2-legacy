5a4d07bb6dc8a1d0a55a31a5d19582a1170d1a87
PAS <pasrc@proton.me>
2026-09-06 21:39:24 -0400

    Only define macOS DEFAULTDIR path on __APPLE__

--- src/doomdef.h
+++ src/doomdef.h
@@ -316,7 +316,7 @@ enum {
 // The XDG Base Directory Specification (*nix) or Application Support (macOS) directory is preferred
 #if (defined (__unix__) || defined (UNIXCOMMON)) && !defined (__CYGWIN__) && !defined (__APPLE__) && !defined(__ANDROID__)
 #define DEFAULTDIR ".srb2_21"
-#else
+#elif defined (__APPLE__)
 #define DEFAULTDIR "srb2_21"
 #endif
 
